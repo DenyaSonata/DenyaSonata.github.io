42cdcec2b13a2a906173ef0aa571188e4a0a8b4e
DenyaSonata <dhenyashu@gmail.com>
2025-01-11 17:03:24 +0700

    .

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function () {
     const header = document.querySelector("#head");
-    const profile = document.querySelector("#home");
-    let triggerHeight = profile.offsetHeight + 130;
+    const profile = document.querySelector("#artworks");
+    let triggerHeight = profile.offsetHeight - 900;
   
     if (window.scrollY > triggerHeight) {
       header.classList.add("header-sticky");
